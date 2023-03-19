89ce1e35f275980cee0a099d42c7d0685d34f841
Parker Tian <park1996@gmail.com>
2023-03-18 20:10:09 -0700

    Modify config file generation script so it is done in parallel

--- simulator/generate_rep_applications_configs.py
+++ simulator/generate_rep_applications_configs.py
@@ -1,4 +1,6 @@
 import os
+import threading
+import time
 
 benchmark_suites = ["chai", "darknet", "hashjoin", "ligra", "phoenix", "polybench", "rodinia", "stream"]
 #HPCG is included in the representative functions list but requires configuration
@@ -6,8 +8,22 @@ benchmark_suites = ["chai", "darknet", "hashjoin", "ligra", "phoenix", "polybenc
 generation_script = "scripts/generate_config_files.py"
 prefetch_generation_script = "generate_pim_prefetch_config_files.py" #Prefetch is special
 command_files = os.getcwd()+"/command_files/"
-for benchmark in benchmark_suites:
+maximum_thread = 70
+threads = []
+def generate_benchmark_config(benchmark):
     os.system("python2 " + generation_script + " " + command_files + benchmark + "_cf")
-
-for benchmark in benchmark_suites:
+def generate_prefetch_benchmark_config(benchmark):
     os.system("python2 " + prefetch_generation_script + " " + command_files + benchmark + "_cf")
+for benchmark in benchmark_suites:
+    current_thread = threading.Thread(target = generate_benchmark_config, args = (benchmark,))
+    threads.append(current_thread)
+    current_thread.start()
+    while threading.active_count() >= maximum_thread + 1:
+        time.sleep(1)
+    current_thread = threading.Thread(target = generate_prefetch_benchmark_config, args = (benchmark,))
+    threads.append(current_thread)
+    current_thread.start()
+    while threading.active_count() >= maximum_thread + 1:
+        time.sleep(1)
+for thread in threads:
+    thread.join()
