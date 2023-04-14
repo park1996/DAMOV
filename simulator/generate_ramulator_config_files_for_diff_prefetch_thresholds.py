1fc5a78f766fcee5661009ef6ace8ab2aad355df
Parker Tian <park1996@gmail.com>
2023-04-13 22:41:57 -0700

    Update ramulator config generation script

--- simulator/generate_ramulator_config_files_for_diff_prefetch_thresholds.py
+++ simulator/generate_ramulator_config_files_for_diff_prefetch_thresholds.py
@@ -1,10 +1,12 @@
 import sys
 import os
 import errno
-from batch_prefetcher_generator import get_hops_thresholds, get_count_thresholds
+from batch_prefetcher_generator import get_hops_thresholds, get_count_thresholds, get_debug_flags, get_prefetcher_types
 
 hops_thresholds = get_hops_thresholds()
 count_thresholds = get_count_thresholds()
+prefetcher_types = get_prefetcher_types()
+debug_flags = get_debug_flags()
 ramulator_config_dir = os.path.join(os.getcwd(), "ramulator-configs")
 template_dir = os.path.join(ramulator_config_dir, "HMC-SwapSubscriptionPF-config-template.cfg")
 
@@ -21,12 +23,17 @@ mkdir_p(os.path.join(ramulator_config_dir, "prefetcher"))
 
 for hops in hops_thresholds:
     for count in count_thresholds:
-        output_dir = os.path.join(ramulator_config_dir, "prefetcher", "HMC-SwapSubscriptionPF-"+str(hops)+"h"+str(count)+"c-config.cfg")
-        with open(template_dir, "r") as ins:
-            config_file = open(output_dir,"w")
-            for line in ins:
-                line = line.replace("COUNT_THRESHOLD_NUMBER", str(count))
-                line = line.replace("HOPS_THRESHOLD_NUMBER", str(hops))
-                config_file.write(line)
-            config_file.close()
-        ins.close()
+        for debug_flag in debug_flags:
+            for prefetcher_type in prefetcher_types:
+                debug_tag = "DebugOn" if debug_flag == "true" else "DebugOff"
+                output_dir = os.path.join(ramulator_config_dir, "prefetcher", "HMC-SwapSubscriptionPF-"+prefetcher_type+"-"+debug_tag+"-"+str(hops)+"h"+str(count)+"c-config.cfg")
+                with open(template_dir, "r") as ins:
+                    config_file = open(output_dir,"w")
+                    for line in ins:
+                        line = line.replace("PREFETCHER_TYPE", prefetcher_type)
+                        line = line.replace("DEBUG_FLAG", debug_tag)
+                        line = line.replace("COUNT_THRESHOLD_NUMBER", str(count))
+                        line = line.replace("HOPS_THRESHOLD_NUMBER", str(hops))
+                        config_file.write(line)
+                    config_file.close()
+                ins.close()
