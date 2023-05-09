06ab4f35709afc45045d543f4ed61d2937113916
Parker Tian <park1996@gmail.com>
2023-05-08 17:22:06 -0700

    Fix mass compile code

--- compile_rep_workloads.py
+++ compile_rep_workloads.py
@@ -4,16 +4,16 @@ import subprocess
 benchmark_suites = ["chai-cpu", "Darknet", "multicore-hashjoins-0.1", "ligra", "phoenix", "PolyBench-ACC", "rodinia_3.1", "STREAM"]
 root_dir = os.getcwd()
 for benchmark in benchmark_suites:
-    os.chdir(root_dir+"/workload/"+benchmark)
+    os.chdir(root_dir+"/workloads/"+benchmark)
     return_code = subprocess.call(["python2", "compile.py"])
     #print "The return code of compiling benchmark " + benchmark + " is " + str(return_code)
     if return_code is not 0:
         print "Error when compiling benchmark: " + benchmark
         exit(1)
-os.chdir(root_dir+"/workload/hpcg")
+os.chdir(root_dir+"/workloads/hpcg")
 # add -DHPGSym into setup/Make.GCC_OMP file, make build directory, run configure GCC_OMP in it
 return_code = subprocess.call(["python2", "compile.py"])
-os.chdir(root_dir+"/workload/parsec-3.0")
+os.chdir(root_dir+"/workloads/parsec-3.0")
 # Install MPI interface by using sudo apt install mpich
 return_code = subprocess.call(["python2", "compile_parsec.py"])
 if return_code is not 0:
